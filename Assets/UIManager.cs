791ee0c1886ecb42de1458f52c4b0ee56df9285a
Grant Wilson <granthaldanewilson836@gmail.com>
2017-10-28 00:15:01 +0200

    Working on room number validation

--- Assets/UIManager.cs
+++ Assets/UIManager.cs
@@ -1,12 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour {
 
     public static UIManager instance;
     public UICamera cam;
+    public InputField RegisterPassword;
+    public InputField RegisterPasswordConfirm;
+    public Text RegisterErrorMessage;
+	public Text RoomErrorMessage;
+	public Text RoomNumber;
 
     private void Awake()
     {
@@ -36,11 +42,49 @@ public class UIManager : MonoBehaviour {
 
     //register join buttons
     public void RegisterClicked(){
-        
+        if (isPasswordsMatching()){
+            //log in
+        }
     }
 
     public void LogInClicked(){
         cam.LookAtCreateRoom();
+    }
+
+    public void checkRoomNumber(){
+        if (RoomNumber.text.Equals("1") || RoomNumber.text.Equals("2") || RoomNumber.text.Equals("3") || RoomNumber.text.Equals("4")){
+			RoomErrorMessage.text = "";
+            RoomErrorMessage.enabled = false;
+        } else {
+			RoomErrorMessage.text = "Please enter a room number between 1-4.";
+			RoomErrorMessage.enabled = true;
+        }
+    }
+
+	public bool isPasswordsMatching()
+	{
+		if (!RegisterPassword.text.Equals(RegisterPasswordConfirm))
+		{
+			RegisterErrorMessage.enabled = true;
+			RegisterErrorMessage.text = "Passwords do not match.";
+            return false;
+		}
+		else
+		{
+			RegisterErrorMessage.enabled = false;
+			RegisterPassword.text = "";
+            return true;
+		}
+	}
+
+    public void PasswordsMatching(){
+        if (!RegisterPassword.text.Equals(RegisterPasswordConfirm.text)){
+            RegisterErrorMessage.enabled = true;
+            RegisterErrorMessage.text = "Passwords do not match.";
+        } else {
+            RegisterErrorMessage.enabled = false;
+            RegisterErrorMessage.text = "";
+		}
     }
 
     //join room buttons
